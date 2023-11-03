251c94ba7ad98b7b55d29521c82d6dbc2373cc36
Binh Nguyen <binhnguyen@eduroam-169-233-194-180.ucsc.edu>
2023-11-02 18:48:45 -0700

    forgot counter output wire

--- src/tt_um_stdp.v
+++ src/tt_um_stdp.v
@@ -17,7 +17,10 @@ module tt_um_stdp (
 assign uio_oe = 8'b11111111;
 assign uio_out[6:0] = 6'd0;
 
-// instatntiate lif neuron
+// instantiate lif neuron
 lif lif1(.current(ui_in), .clk(clk), .rst_n(rst_n), .spike(uio_out[7]), .state(uo_out));
+
+// instantiate stdp
+//stdp stdp1(.current(ui_in), .clk(clk), .rst_n(rst_n), .spike(uio_out[7]), .state(uo_out));
 
 endmodule
