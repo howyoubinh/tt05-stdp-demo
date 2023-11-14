c405bcbe658d0b58837a97026b9bf05e74ab8d83
Binh Nguyen <binhnguyen@eduroam-169-233-149-109.ucsc.edu>
2023-11-13 18:47:16 -0800

    used for loop to update all 5 presynaptic neurons and included spike times and time diffs for each

--- src/stdp2.v
+++ src/stdp2.v
@@ -5,52 +5,80 @@ module stdp2 (
     input wire       rst_n, // reset signal
     input wire [4:0] pre_spike, // pre-synaptic spike
     input wire       post_spike, // post-synaptic spike
-    output reg [7:0]time_diff, // 8-bit output time difference
-    output reg      update_w_flag, // 1 bit update flag
-    output wire [7:0]weight
+    output reg [4:0] time_diff_out, // 8-bit output time difference
+    output reg       update_w_flag, // 1 bit update flag
+    output wire [4:0]weight
 );
 
-// local variables
-reg [15:0] pre_spike_time;
-reg [15:0] post_spike_time;
-reg [15:0] weight_local;
+// Number of presynaptic neurons
+localparam NUM_PRE_NEURONS = 5;
+
+// internal signals to store spike times
+reg [7:0] pre_spike_times [0:NUM_PRE_NEURONS-1]; // 5 presynaptic neuron times
+reg [7:0] post_spike_time; // 1 postsynaptic neuron time
+
+// internal signal for time differences
+reg [7:0] time_diffs [0:NUM_PRE_NEURONS-1]; // post - pre
+
+// internal signals for weights
+reg [7:0] weights [0:NUM_PRE_NEURONS-1];
+
+// internal signal for weight update flag
+reg update_w_flag_internal;
+
 
 // increment pre_spike_time and post_spike_time
 always @(posedge clk) begin
-    if (!rst_n) begin // initialize variables
-        pre_spike_time <= 16'b0;
-        post_spike_time <= 16'b0;
+    if (!rst_n) begin // initialize signals
+        pre_spike_times <= '0;
+        post_spike_time <= 8'b0;
+        time_diffs <= '0;
+        weights <= '0;
+        update_w_flag_internal <= 1'b0;
     end else begin
-        pre_spike_time <= pre_spike ? 16'b0 : pre_spike_time + 1; // if spike is true, reset timer else increment timer
-        post_spike_time <= post_spike ? 16'b0 : post_spike_time + 1; // change this back to 1 to pass gds
+
+        // update spikes for presynaptic neurons
+        for (int i = 0; i < NUM_PRE_NEURONS; i = i + 1) begin
+            if (pre_spike[i]) begin
+                pre_spike_times[i] <= 8'b0;
+            end else begin
+                pre_spike_times[i] = pre_spike_times[i] + 1; // increment presynaptic timer
+            end
+        end
+
+        // update spike time for postsynaptic neuron
+        if (post_spike) begin
+            post_spike_time <= 8'b0;
+        end else begin
+            post_spike_time = post_spike_time + 1; // increment postsynaptic timer
+        end
+
+        // calculate time diff and update weights
+        for (int i = 0; i < NUM_PRE_NEURONS; i = i + 1) begin
+            time_diffs[i] <= post_spike_time - pre_spike_time[i];
+            weights[i] <= calculate_weight(time_diffs[i]); // calculate_weight function takes time diff as input
+        end
+
+        // check if weights need to be updated
+        update_w_flag_internal <= (|time_diffs); // check if any time_diffs are present
     end
 end
 
-// calculate time diff whenever time changes (only LTP)
-always @(posedge clk) begin
-    if (!rst_n) begin
-        time_diff <= 8'b0;
-        update_w_flag <= 1'b0;
-    end else begin
-    time_diff <= post_spike_time - pre_spike_time; // assume pre comes before post (LTP)
-    update_w_flag <= (time_diff > 0) ? 1'b1 : 1'b0; // if time_diff > 0, update_w_flag is true else false
+// assign internal signals to output ports
+assign time_diff_out = time_diffs;
+assign weight_out = weights;
+assign update_w_flag = update_w_flag_internal;
+
+function [7:0] calculate_weight;
+    input [7:0] time_diff;
+    begin
+        // positive time_diff = LTP 
+        
+        // negative time_diff = LTD
+
+        // placeholder for calculate_weight
+        calculate_weight = time_diff; // weight directly proportional to time difference
     end
-end
-
-// LUT for weight update
-always @(posedge clk) begin
-    if (!rst_n) begin
-        weight_local <= 8'b1;
-    end else begin
-        case (update_w_flag)
-            1'b1: weight_local <= (weight_local << 1); // weight * 2
-            1'b0: weight_local <= (weight_local >> 1); // weight / 2
-        endcase
-    end
-end
-
-//spike1 and weight should be same bit width
-
-assign weight = weight_local;
+endfunction
 
 endmodule
