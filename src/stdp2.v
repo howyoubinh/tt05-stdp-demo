ff88870216ed4c754293cdddfd1588c82dc0104d
Binh Nguyen <binhnguyen@Binhs-MBP.routerlogin.net>
2023-11-15 02:30:07 -0800

    changed to 4 presyanptic neurons and wired weights to 8 bit uo_out and 8 bit uio_out

--- src/stdp2.v
+++ src/stdp2.v
@@ -3,28 +3,36 @@
 module stdp2 (
     input wire       clk, // clock signal
     input wire       rst_n, // reset signal
-    input wire [4:0] pre_spike, // pre-synaptic spike
+    // input wire [4:0] pre_spike, // pre-synaptic spike
+    input wire [3:0] pre_spike, // 4 bit pre-synaptic spike
     input wire       post_spike, // post-synaptic spike
     output reg [7:0] time_diff, // 8-bit output time difference
     output reg       update_w_flag, // 1 bit update flag
-    output wire [7:0]weight
+    output wire [15:0]weight // 16 bit weight
 );
 
 // Number of presynaptic neurons
-localparam NUM_PRE_NEURONS = 5;
+localparam NUM_PRE_NEURONS = 4;
 
 // internal signals to store spike times
-reg [7:0] pre_spike_times [0:NUM_PRE_NEURONS-1]; // 5 presynaptic neuron times
+reg [7:0] pre_spike_times [0:NUM_PRE_NEURONS-1]; // 4 presynaptic neuron times
 reg [7:0] post_spike_time; // 1 postsynaptic neuron time
 
 // internal signal for time differences
 reg [7:0] time_diffs [0:NUM_PRE_NEURONS-1]; // post - pre
 
 // internal signals for weights
-reg [7:0] weights [0:NUM_PRE_NEURONS-1];
+reg [3:0] weights [0:NUM_PRE_NEURONS-1]; // 4 bit weights
 
 // internal signal for weight update flag
 reg update_w_flag_internal;
+
+initial begin
+    // dump array word
+    for (int i = 0; i < NUM_PRE_NEURONS; i = i + 1) begin
+        $dumpvars(0, pre_spike_times[i]);
+    end
+end
 
 
 // increment pre_spike_time and post_spike_time
@@ -33,7 +41,7 @@ always @(posedge clk) begin
         for (int i = 0; i < NUM_PRE_NEURONS; i = i + 1) begin
             pre_spike_times[i] <= 8'b0;
             time_diffs[i] <= 8'b0;
-            weights[i] <= 8'b0;
+            weights[i] <= 4'b0;
         end
         post_spike_time <= 8'b0;
         update_w_flag_internal <= 1'b0;
@@ -67,8 +75,12 @@ always @(posedge clk) begin
 end
 
 // assign internal signals to output ports
+
 assign time_diff = time_diffs[0];
-assign weight = weights[0];
+assign weight[15:12] = weights[0];
+assign weight[11:8] = weights[1];
+assign weight[7:4] = weights[2];
+assign weight[3:0] = weights[3];
 assign update_w_flag = update_w_flag_internal;
 
 function [7:0] calculate_weight;
--- src/tt_um_stdp2.v
+++ src/tt_um_stdp2.v
@@ -13,16 +13,19 @@ module tt_um_stdp2 (
 
 // use bidirectionals as outputs
 assign uio_oe = 8'b11111111; // 1 = enable
-assign uio_out[6:0] = 6'd0; // set unused bits to 0
+// assign uio_out[6:0] = 6'd0; // set unused bits to 0
 
 // in
 wire [7:0] in_pre, in_post; // 8-bit input wires
 
 // outs
-wire [4:0]spike_pre; // 5 bit spike vector (1 for each presynaptic neuron)
+// wire [4:0]spike_pre; // 5 bit spike vector (1 for each presynaptic neuron)
+wire [3:0]spike_pre; // 4 bit spike vector (1 for each presynaptic neuron)
 wire      spike_post; // 1-bit output spike
 reg [7:0] state_pre1, state_pre2, state_pre3, state_pre4, state_pre5, state_post; // 8-bit output state
-reg [7:0] time_diff_out, weight_out;
+reg [7:0] time_diff_out;
+// reg [7:0] weight_out;
+wire [15:0] weight_out; // 4x4bit weights
 reg       w_flag_out;
 wire [7:0] threshold_pre1, threshold_pre2, threshold_pre3, threshold_pre4, threshold_pre5, threshold_post;
 
@@ -33,7 +36,7 @@ assign threshold_pre1 = 260;
 assign threshold_pre2 = 220;
 assign threshold_pre3 = 180;
 assign threshold_pre4 = 140;
-assign threshold_pre5 = 100;
+// assign threshold_pre5 = 100;
 assign threshold_post = 200;
 // assign in2 = spike_out1 ? weight_out : 0; // in2 = spike_1 * weight (TO-DO)
 
@@ -41,17 +44,25 @@ assign threshold_post = 200;
 stdp2 stdp(.clk(clk), .rst_n(rst_n), .pre_spike(spike_pre), .post_spike(spike_post), .time_diff(time_diff_out), .update_w_flag(w_flag_out), .weight(weight_out));
 
 // instantiate 5 LIF for each presynaptic neuron
-lif lif_pre1(.current(in_pre), .clk(clk), .rst_n(rst_n), .spike(spike_pre[4]), .state(state_pre1), .threshold_val(threshold_pre1));
-lif lif_pre2(.current(in_pre), .clk(clk), .rst_n(rst_n), .spike(spike_pre[3]), .state(state_pre2), .threshold_val(threshold_pre2));
-lif lif_pre3(.current(in_pre), .clk(clk), .rst_n(rst_n), .spike(spike_pre[2]), .state(state_pre3), .threshold_val(threshold_pre3));
-lif lif_pre4(.current(in_pre), .clk(clk), .rst_n(rst_n), .spike(spike_pre[1]), .state(state_pre4), .threshold_val(threshold_pre4));
-lif lif_pre5(.current(in_pre), .clk(clk), .rst_n(rst_n), .spike(spike_pre[0]), .state(state_pre5), .threshold_val(threshold_pre5));
+lif lif_pre1(.current(in_pre), .clk(clk), .rst_n(rst_n), .spike(spike_pre[3]), .state(state_pre1), .threshold_val(threshold_pre1));
+lif lif_pre2(.current(in_pre), .clk(clk), .rst_n(rst_n), .spike(spike_pre[2]), .state(state_pre2), .threshold_val(threshold_pre2));
+lif lif_pre3(.current(in_pre), .clk(clk), .rst_n(rst_n), .spike(spike_pre[1]), .state(state_pre3), .threshold_val(threshold_pre3));
+lif lif_pre4(.current(in_pre), .clk(clk), .rst_n(rst_n), .spike(spike_pre[0]), .state(state_pre4), .threshold_val(threshold_pre4));
+// lif lif_pre5(.current(in_pre), .clk(clk), .rst_n(rst_n), .spike(spike_pre[0]), .state(state_pre5), .threshold_val(threshold_pre5));
 
 // instantiate 1 LIF for postsynaptic neuron
 lif lif_post(.current(in_post), .clk(clk), .rst_n(rst_n), .spike(spike_post), .state(state_post), .threshold_val(threshold_post));
 
 // assign outputs wires to output to lif2
-assign  uio_out[7] = spike_post;
-assign  uo_out = state_post;
+// assign  uio_out[7] = spike_post;
+// assign  uo_out = state_post;
+
+// wire outputs with 4 bit weights for each of the 4 presynaptic neurons
+// lif_pre1 = weight[15:12]
+// lif_pre2 = weight[11:8]
+// lif_pre3 = weight[7:4]
+// lif_pre4 = weight[3:0]
+assign uo_out = weight_out[15:8];
+assign uio_out = weight_out[7:0];
 
 endmodule
