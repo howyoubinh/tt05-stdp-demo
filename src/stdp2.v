0802d2fc6c93cede22404d182aeb88a3490db49b
Binh Nguyen <binhnguyen@eduroam-169-233-217-40.ucsc.edu>
2023-11-15 17:23:01 -0800

    go back to working code

--- src/stdp2.v
+++ src/stdp2.v
@@ -74,9 +74,9 @@ always @(posedge clk) begin
 
         // calculate time diff and update weights
         for (int i = 0; i < NUM_PRE_NEURONS; i = i + 1) begin
-            time_diffs[i] <= (post_spike_time - pre_spike_times[i] > 0) ? 4'b1 : 4'b0;
+            // time_diffs[i] <= (post_spike_time - pre_spike_times[i] > 0) ? 4'b1 : 4'b0; // failing gds
             // time_diffs[i] <= post_spike_time - pre_spike_times[i]; // this is failing gds
-            // time_diffs[i] <= pre_spike_times[i]; // this works
+            time_diffs[i] <= pre_spike_times[i]; // this works
 
             // if (time_diffs[i] < 0) begin
             //     time_diffs[i] = 4'b0; // clamp lower bound to 0
